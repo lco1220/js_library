188cd1b6ab0f25022317566d2aad51c1920a7c2b
lco1220 <leorieleco@gmail.com>
2019-08-29 16:09:17 +0800

    Delete code in render()

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var myLibrary = [
   {title: 'A Game of Thrones', author: 'George R. R. Martin', genre: 'Epic Fantasy', read: false}
 ];
 
-function Book(title, author, genre, read) {
-  this.title = title;
-  this.author = author;
-  this.genre = genre;
-  this.read = read;
+class Book {
+  constructor(title, author, genre, read) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary(Book) {
@@ -23,31 +25,7 @@ function render(book) {
   var bookTable = document.querySelector('#bookList');
   var row = document.createElement('tr');
 
-    if(book.read) {
-      row.innerHTML = 
-        ` <td>${book.title}</td>
-          <td>${book.author}</td>
-          <td>${book.genre}</td>
-          <td>
-          <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="read" checked>
-            <label class="custom-control-label" for="read"></label>
-          </div> </td>`;
-    } else {
-      row.innerHTML = 
-        ` <td>${book.title}</td>
-          <td>${book.author}</td>
-          <td>${book.genre}</td>
-          <td>
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="read">
-              <label class="custom-control-label" for="read"></label>
-            </div>
-          </td>`;
-    }
-  
 
-  bookTable.appendChild(row);
 }
 
 displayBooks(myLibrary);
